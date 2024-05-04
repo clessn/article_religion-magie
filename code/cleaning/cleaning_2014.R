6c7a76365f35ce81dcbc83f32b77bc21ddf8f14c
capel201 <cam.pell@me.com>
2024-05-03 21:38:25 -0400

    ajout clinine 2014

--- code/cleaning/cleaning_2014.R
+++ code/cleaning/cleaning_2014.R
@@ -1,3 +1,9 @@
 data <- haven::read_sav("_SharedFolder_article_religion-magie/Data/MJU001 - PONDÉRÉ N=2000.Sav")
 
 codebook <- sondr::sav_to_codebook(data)
+
+data <- data %>% 
+  select(Q2_5, Q23, Q28, Q29, Q32, Q33, Q34, Q48, Q36, Q39)
+
+
+## Q2_5 ------------------------------------------------------------------------
