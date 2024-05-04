1388dc85c4d4d5cc3f3700d7a574d178b9e719d6
capel201 <cam.pell@me.com>
2024-05-03 21:24:47 -0400

    oublier de push oups

---
+++ code/cleaning/cleaning_quorum_1.R
@@ -0,0 +1,9 @@
+
+library(dplyr)
+
+data <- haven::read_sav("/Users/camillepelletier/Dropbox/article_religion-magie/_SharedFolder_article_religion-magie/Data/quorum_1/ULAQUORUM.Sav", encoding = "latin1")
+
+codebook <- sondr::sav_to_codebook(data)
+
+data <- data %>% 
+  select(PROV, CP, SEXE, YOB, AGE, LANGU, Q47, Q48, Q51, Q52, Q54, Q10B_A1, Q4, Q61, Q62, Q49, Q2B_5, Q12_4_inv, Q17_3_inv, Q17_7, Q23, Q35, Q37_2)
