f4d585464a0d1ca316437a398a3447799a3b9a3a
LaurenceOMFoisy <mail@mfoisy.com>
2024-05-04 00:53:47 -0400

    termino clinin

--- code/cleaning/cleaning_2014.R
+++ code/cleaning/cleaning_2014.R
@@ -51,22 +51,25 @@ table(data_clean$ses_female)
 attributes(data_raw$YOB)
 table(data_raw$YOB)
 data_clean$ses_age <- NA
-data_clean$ses_age <- 2024 - data_raw$YOB
+year_map <- setNames(as.character(1900:2002), 1:103)
+data_raw$yob_label <- year_map[as.character(data_raw$YOB)]
+data_raw$yob_label <- factor(data_raw$yob_label)
+data_clean$ses_age <- as.numeric(data_raw$yob_label)
 table(data_clean$ses_age)
 
 ## Age group
 
 data_clean$ses_age_group <- NA
 data_clean$ses_age_group[data_raw$YOB > 2006] <- "under_18"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 18:24] <- "18_24"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 25:34] <- "25_34"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 35:44] <- "35_44"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 45:54] <- "45_54"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 55:59] <- "55_59"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 60:64] <- "60_64"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 65:74] <- "65_74"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 75:84] <- "75_84"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 85:125] <- "over_85"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 18:24] <- "18_24"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 25:34] <- "25_34"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 35:44] <- "35_44"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 45:54] <- "45_54"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 55:59] <- "55_59"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 60:64] <- "60_64"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 65:74] <- "65_74"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 75:84] <- "75_84"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 85:125] <- "over_85"
 data_clean$ses_age_group <- factor(data_clean$ses_age_group, levels = c("under_18",
                                                                         "18_24",
                                                                         "25_34",
@@ -80,3 +83,144 @@ data_clean$ses_age_group <- factor(data_clean$ses_age_group, levels = c("under_1
 table(data_clean$ses_age_group)
 
 
+## LANGU -----------------------------------------------------------------------
+
+attributes(data_raw$LANGU)
+table(data_raw$LANGU)
+data_clean$ses_language <- NA
+data_clean$ses_language[data_raw$LANGU == 1] <- "fr"
+data_clean$ses_language[data_raw$LANGU == 2] <- "en"
+data_clean$ses_language[data_raw$LANGU == 3] <- "other"
+table(data_clean$ses_language)
+
+
+## Q2_5 ------------------------------------------------------------------------
+
+attributes(data_raw$Q2_5)
+table(data_raw$Q2_5)
+data_clean$horoscope <- NA
+data_clean$horoscope[data_raw$Q2_5 == 1] <- 0
+data_clean$horoscope[data_raw$Q2_5 == 2] <- 0.25
+data_clean$horoscope[data_raw$Q2_5 == 3] <- 0.5
+data_clean$horoscope[data_raw$Q2_5 == 4] <- 0.75
+data_clean$horoscope[data_raw$Q2_5 == 5] <- 1
+table(data_clean$horoscope)
+
+## Q23 -------------------------------------------------------------------------
+
+attributes(data_raw$Q23)
+table(data_raw$Q23)
+data_clean$how_many_immigrants <- NA
+thresholds <- c(0, 200000, 300000, 400000, 1000000, Inf)
+labels <- c(1, 2, 3, 4, 5)
+data_raw$immigrants_likert <- cut(data_raw$Q23, breaks = thresholds, labels = labels, include.lowest = TRUE, right = FALSE)
+data_clean$how_many_immigrants <- (as.numeric(data_raw$immigrants_likert) -1) / 4
+table(data_clean$how_many_immigrants)
+
+## Q28 -------------------------------------------------------------------------
+
+attributes(data_raw$Q28)
+table(data_raw$Q28)
+data_clean$religion_member_of_church <- NA
+data_clean$religion_member_of_church[data_raw$Q28 == 1 | data_raw$Q28 == 96] <- 0
+data_clean$religion_member_of_church[data_raw$Q28 == 2] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 3] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 4] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 5] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 6] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 7] <- 1
+data_clean$religion_member_of_church[data_raw$Q28 == 8] <- 1
+table(data_clean$religion_member_of_church)
+
+## Q29 -------------------------------------------------------------------------
+
+attributes(data_raw$Q29)
+table(data_raw$Q29)
+data_clean$religion_attached_to_church <- NA
+data_clean$religion_attached_to_church <- (data_raw$Q29 - 1) / 4
+table(data_clean$religion_attached_to_church)
+
+## Q32 -------------------------------------------------------------------------
+
+attributes(data_raw$Q32)
+table(data_raw$Q32)
+data_clean$ses_sexual_orientation <- NA
+data_clean$ses_sexual_orientation[data_raw$Q32 == 1] <- "heterosexual"
+data_clean$ses_sexual_orientation[data_raw$Q32 == 2] <- "homomosexual"
+data_clean$ses_sexual_orientation[data_raw$Q32 == 3] <- "bisexual"
+data_clean$ses_sexual_orientation[data_raw$Q32 == 4] <- "other"
+table(data_clean$ses_sexual_orientation)
+
+## Q33 -------------------------------------------------------------------------
+
+attributes(data_raw$Q33)
+table(data_raw$Q33)
+data_clean$ses_marital_status <- NA
+data_clean$ses_marital_status[data_raw$Q33 == 1] <- "married"
+data_clean$ses_marital_status[data_raw$Q33 == 2] <- "living_with_partner"
+data_clean$ses_marital_status[data_raw$Q33 == 3] <- "single"
+data_clean$ses_marital_status[data_raw$Q33 == 4] <- "divorced"
+data_clean$ses_marital_status[data_raw$Q33 == 5] <- "separated"
+data_clean$ses_marital_status[data_raw$Q33 == 6] <- "widowed"
+data_clean$ses_marital_status[data_raw$Q33 == 7] <- "never_married"
+data_clean$ses_marital_status[data_raw$Q33 == 8] <- "dk"
+data_clean$ses_marital_status <- factor(data_clean$ses_marital_status, levels = c("married", "living_with_partner", "single", "divorced", "separated", "widowed", "never_married", "dk"))
+table(data_clean$ses_marital_status)
+
+## Q34 -------------------------------------------------------------------------
+
+attributes(data_raw$Q34)
+table(data_raw$Q34)
+data_clean$ses_ethnicity <- NA
+data_clean$ses_ethnicity[data_raw$Q34 == 1] <- "white"
+data_clean$ses_ethnicity[data_raw$Q34 == 2] <- "black"
+data_clean$ses_ethnicity[data_raw$Q34 == 3] <- "native"
+data_clean$ses_ethnicity[data_raw$Q34 == 4] <- "asian"
+data_clean$ses_ethnicity[data_raw$Q34 == 5] <- "hispanic"
+data_clean$ses_ethnicity[data_raw$Q34 == 6] <- "arab"
+table(data_clean$ethnicity)
+
+## Q48 -------------------------------------------------------------------------
+
+attributes(data_raw$Q48)
+table(data_raw$Q48)
+data_clean$ses_born_canada <- NA
+data_clean$ses_born_canada[data_raw$Q48 == 1] <- 1
+data_clean$ses_born_canada[data_raw$Q48 != 1] <- 0
+table(data_clean$ses_born_canada)
+
+## Q36 -------------------------------------------------------------------------
+
+attributes(data_raw$Q36)
+table(data_raw$Q36)
+data_clean$ses_education <- NA
+data_clean$ses_education[data_raw$Q36 == 1] <- "before_high_school"
+data_clean$ses_education[data_raw$Q36 == 2] <- "before_high_school"
+data_clean$ses_education[data_raw$Q36 == 3] <- "before_high_school"
+data_clean$ses_education[data_raw$Q36 == 4] <- "before_high_school"
+data_clean$ses_education[data_raw$Q36 == 5] <- "high_school"
+data_clean$ses_education[data_raw$Q36 == 6] <- "high_school"
+data_clean$ses_education[data_raw$Q36 == 7] <- "cegep_college"
+data_clean$ses_education[data_raw$Q36 == 8] <- "cegep_college"
+data_clean$ses_education[data_raw$Q36 == 9] <- "undergraduate"
+data_clean$ses_education[data_raw$Q36 == 10] <- "graduate"
+data_clean$ses_education[data_raw$Q36 == 11] <- "graduate"
+data_clean$ses_education <- factor(data_clean$ses_education, levels = c("before_high_school", "high_school", "cegep_college", "undergraduate", "graduate"))
+table(data_clean$ses_education)
+
+## Q39 -------------------------------------------------------------------------
+
+attributes(data_raw$Q39)
+table(data_raw$Q39)
+data_clean$ses_occupation <- NA
+data_clean$ses_occupation[data_raw$Q39 == 1] <- "employed"
+data_clean$ses_occupation[data_raw$Q39 == 2] <- "employed"
+data_clean$ses_occupation[data_raw$Q39 == 3] <- "student"
+data_clean$ses_occupation[data_raw$Q39 == 4] <- "retired"
+data_clean$ses_occupation[data_raw$Q39 == 5] <- "unemployed"
+data_clean$ses_occupation[data_raw$Q39 == 6] <- "unemployed"
+table(data_clean$ses_occupation)
+
+## Save ------------------------------------------------------------------------
+
+saveRDS(data_clean, "_SharedFolder_article_religion-magie/Data/data_clean/data_2014.RDS")
--- code/cleaning/cleaning_quorum_1.R
+++ code/cleaning/cleaning_quorum_1.R
@@ -51,22 +51,25 @@ table(data_clean$ses_female)
 attributes(data_raw$YOB)
 table(data_raw$YOB)
 data_clean$ses_age <- NA
-data_clean$ses_age <- 2024 - data_raw$YOB
+year_map <- setNames(as.character(1900:2002), 1:103)
+data_raw$yob_label <- year_map[as.character(data_raw$YOB)]
+data_raw$yob_label <- factor(data_raw$yob_label)
+data_clean$ses_age <- as.numeric(data_raw$yob_label)
 table(data_clean$ses_age)
 
 ## Age group
 
 data_clean$ses_age_group <- NA
 data_clean$ses_age_group[data_raw$YOB > 2006] <- "under_18"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 18:24] <- "18_24"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 25:34] <- "25_34"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 35:44] <- "35_44"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 45:54] <- "45_54"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 55:59] <- "55_59"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 60:64] <- "60_64"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 65:74] <- "65_74"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 75:84] <- "75_84"
-data_clean$ses_age_group[(2024 - data_raw$YOB) %in% 85:125] <- "over_85"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 18:24] <- "18_24"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 25:34] <- "25_34"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 35:44] <- "35_44"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 45:54] <- "45_54"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 55:59] <- "55_59"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 60:64] <- "60_64"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 65:74] <- "65_74"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 75:84] <- "75_84"
+data_clean$ses_age_group[as.numeric(data_raw$yob_label) %in% 85:125] <- "over_85"
 data_clean$ses_age_group <- factor(data_clean$ses_age_group, levels = c("under_18",
                                                                         "18_24",
                                                                         "25_34",
@@ -78,3 +81,197 @@ data_clean$ses_age_group <- factor(data_clean$ses_age_group, levels = c("under_1
                                                                         "75_84",
                                                                         "over_85"))
 table(data_clean$ses_age_group)
+
+
+# LANGU
+
+attributes(data_raw$LANGU)
+table(data_raw$LANGU)
+data_clean$ses_language <- NA
+data_clean$ses_language[data_raw$LANGU == 1] <- "fr"
+data_clean$ses_language[data_raw$LANGU == 2] <- "en"
+data_clean$ses_language[data_raw$LANGU == 3] <- "other"
+table(data_clean$ses_language)
+
+# Q47
+
+attributes(data_raw$Q47)
+table(data_raw$Q47)
+data_clean$ses_marital_status <- NA
+data_clean$ses_marital_status[data_raw$Q47 == 1] <- "married"
+data_clean$ses_marital_status[data_raw$Q47 == 2] <- "living_with_partner"
+data_clean$ses_marital_status[data_raw$Q47 == 3] <- "single"
+data_clean$ses_marital_status[data_raw$Q47 == 4] <- "divorced"
+data_clean$ses_marital_status[data_raw$Q47 == 5] <- "separated"
+data_clean$ses_marital_status[data_raw$Q47 == 6] <- "widowed"
+data_clean$ses_marital_status[data_raw$Q47 == 7] <- "never_married"
+data_clean$ses_marital_status[data_raw$Q47 == 8] <- "dk"
+data_clean$ses_marital_status <- factor(data_clean$ses_marital_status, levels = c("married", "living_with_partner", "single", "divorced", "separated", "widowed", "never_married", "dk"))
+table(data_clean$ses_marital_status)
+
+# Q48
+
+attributes(data_raw$Q48)
+table(data_raw$Q48)
+data_clean$ses_born_canada <- NA
+data_clean$ses_born_canada[data_raw$Q48 == 1] <- 1
+data_clean$ses_born_canada[data_raw$Q48 != 1] <- 0
+table(data_clean$ses_born_canada)
+
+# Q51
+
+attributes(data_raw$Q51)
+table(data_raw$Q51)
+data_clean$ses_education <- NA
+data_clean$ses_education[data_raw$Q51 == 1] <- "before_high_school"
+data_clean$ses_education[data_raw$Q51 == 2] <- "before_high_school"
+data_clean$ses_education[data_raw$Q51 == 3] <- "before_high_school"
+data_clean$ses_education[data_raw$Q51 == 4] <- "before_high_school"
+data_clean$ses_education[data_raw$Q51 == 5] <- "high_school"
+data_clean$ses_education[data_raw$Q51 == 6] <- "high_school"
+data_clean$ses_education[data_raw$Q51 == 7] <- "cegep_college"
+data_clean$ses_education[data_raw$Q51 == 8] <- "cegep_college"
+data_clean$ses_education[data_raw$Q51 == 9] <- "undergraduate"
+data_clean$ses_education[data_raw$Q51 == 10] <- "graduate"
+data_clean$ses_education[data_raw$Q51 == 11] <- "graduate"
+data_clean$ses_education <- factor(data_clean$ses_education, levels = c("before_high_school", "high_school", "cegep_college", "undergraduate", "graduate"))
+table(data_clean$ses_education)
+
+# Q52
+
+attributes(data_raw$Q52)
+table(data_raw$Q52)
+data_clean$ses_sexual_orientation <- NA
+data_clean$ses_sexual_orientation[data_raw$Q52 == 1] <- "heterosexual"
+data_clean$ses_sexual_orientation[data_raw$Q52 == 2] <- "bisexual"
+data_clean$ses_sexual_orientation[data_raw$Q52 == 3] <- "homosexual"
+data_clean$ses_sexual_orientation[data_raw$Q52 == 4 | data_raw$Q52 == 5] <- "other"
+data_clean$ses_sexual_orientation <- factor(data_clean$ses_sexual_orientation, levels = c("heterosexual", "bisexual", "homosexual", "other")) 
+table(data_clean$ses_sexual_orientation)
+
+# Q54
+
+attributes(data_raw$Q54)
+table(data_raw$Q54)
+data_clean$ses_occupation <- NA
+data_clean$ses_occupation[data_raw$Q54 == 1] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 2] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 3] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 4] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 5] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 6] <- "employed"
+data_clean$ses_occupation[data_raw$Q54 == 7] <- "unemployed"
+data_clean$ses_occupation[data_raw$Q54 == 8] <- "retired"
+data_clean$ses_occupation[data_raw$Q54 == 9] <- "student"
+data_clean$ses_occupation <- factor(data_clean$ses_occupation, levels = c("white_collar", "blue_collar", "unemployed", "retired", "student"))
+table(data_clean$ses_occupation)
+
+# Q10B_A1
+
+attributes(data_raw$Q10B_A1)
+table(data_raw$Q10B_A1)
+data_clean$horoscope <- NA
+data_clean$horoscope <- (data_raw$Q10B_A1 - 1) / 4
+table(data_clean$horoscope)
+
+# Q4
+
+attributes(data_raw$Q4)
+table(data_raw$Q4)
+data_clean$how_many_immigrants <- NA
+data_clean$how_many_immigrants <- (data_raw$Q4 - 1) / 4
+table(data_clean$how_many_immigrants)
+
+# Q61
+
+attributes(data_raw$Q61)
+table(data_raw$Q61)
+data_clean$religion_member_of_church <- NA
+data_clean$religion_member_of_church[data_raw$Q61 == 1] <- 1
+data_clean$religion_member_of_church[data_raw$Q61 != 1] <- 0
+table(data_clean$religion_member_of_church)
+
+# Q62
+
+attributes(data_raw$Q62)
+table(data_raw$Q62)
+data_clean$religion_attached_to_church <- NA
+data_clean$religion_attached_to_church <- (data_raw$Q62 - 1) / 4
+table(data_clean$religion_attached_to_church)
+
+# Q49
+
+attributes(data_raw$Q49)
+table(data_raw$Q49)
+data_clean$ses_ethnicity <- NA
+data_clean$ses_ethnicity[data_raw$Q49 == 1] <- "white"
+data_clean$ses_ethnicity[data_raw$Q49 == 2] <- "black"
+data_clean$ses_ethnicity[data_raw$Q49 == 3] <- "native"
+data_clean$ses_ethnicity[data_raw$Q49 == 4] <- "asian"
+data_clean$ses_ethnicity[data_raw$Q49 == 5] <- "hispanic"
+data_clean$ses_ethnicity[data_raw$Q49 == 6] <- "arab"
+table(data_clean$ethnicity)
+
+# Q2B_5
+
+attributes(data_raw$Q2B_5)
+table(data_raw$Q2B_5)
+data_clean$covid_limiting_freedom_is_necessary <- NA
+data_clean$covid_limiting_freedom_is_necessary <- (data_raw$Q2B_5 - 1) / 4
+table(data_clean$covid_limiting_freedom_is_necessary)
+
+# Q12_4
+
+attributes(data_raw$Q12_4)
+table(data_raw$Q12_4)
+data_clean$covid_only_vulnerable_people <- NA
+data_clean$covid_only_vulnerable_people <- (data_raw$Q12_4 - 1) / 4
+data_clean$covid_only_vulnerable_people <- sondr::finverser(data_clean$covid_only_vulnerable_people)
+table(data_clean$covid_only_vulnerable_people)
+
+# Q17_3
+
+attributes(data_raw$Q17_3)
+table(data_raw$Q17_3)
+data_clean$covid_not_afraid_of_dying <- NA
+data_clean$covid_not_afraid_of_dying <- (data_raw$Q17_3 - 1) / 4
+data_clean$covid_not_afraid_of_dying <- sondr::finverser(data_clean$covid_not_afraid_of_dying)
+table(data_clean$covid_not_afraid_of_dying)
+
+# Q17_7
+
+attributes(data_raw$Q17_7)
+table(data_raw$Q17_7)
+data_clean$covid_pandemic_is_nature_revenge <- NA
+data_clean$covid_pandemic_is_nature_revenge <- (data_raw$Q17_7 - 1) / 4
+table(data_clean$covid_pandemic_is_nature_revenge)
+
+# Q23
+
+attributes(data_raw$Q23)
+table(data_raw$Q23)
+data_clean$covid_feeling <- NA
+data_clean$covid_feeling <- data_raw$Q23 / 10
+table(data_clean$covid_feeling)
+
+# Q35
+
+attributes(data_raw$Q35)
+table(data_raw$Q35)
+data_clean$covid_long_run <- NA
+data_clean$covid_long_run <- data_raw$Q35 / 10
+table(data_clean$covid_long_run)
+
+# Q37_2
+
+attributes(data_raw$Q37_2)
+table(data_raw$Q37_2)
+data_clean$covid_pandemic_more_serious <- NA
+data_clean$covid_pandemic_more_serious <- (data_raw$Q37_2 - 1) / 4
+table(data_clean$covid_pandemic_more_serious)
+
+# Save data
+
+saveRDS(data_clean, "_SharedFolder_article_religion-magie/Data/data_clean/data_quorum_1.RDS")
+
+
