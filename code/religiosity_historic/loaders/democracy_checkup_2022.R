48f603f4f976753cb43fc22b56def1b68f2a7b03
hubcad25 <hubertcadieux@gmail.com>
2024-08-07 13:32:52 -0400

    modif template

--- code/religiosity_historic/loaders/democracy_checkup_2022.R
+++ code/religiosity_historic/loaders/democracy_checkup_2022.R
@@ -3,8 +3,7 @@ library(dplyr)
 
 # Load Raw Data -------------------------------------------------------------------
 
-raw_data
-
+raw_data <- haven::read_sav("_SharedFolder_article_religion-magie/Data/religiosity_historic/lake/democracy_checkup_2022.sav", encoding = "latin1")
 
 # Create Clean Data ------------------------------------------------------
 
@@ -14,22 +13,34 @@ clean_data <- data.frame(
 
 # Clean relevant variables -----------------------------------------------
 
-
 ## Quebec-Canada -----------------------------------------------------------------
 
 ### name this variable "subgroup"
 #### categories : qc, can
 
+attributes(raw_data$dc22_province)
+table(raw_data$dc22_province, useNA = "always")
+clean_data$subgroup <- NA
+clean_data$subgroup[raw_data$dc22_province == 11] <- "qc"
+clean_data$subgroup[raw_data$dc22_province != 11] <- "can"
+table(clean_data$subgroup)
 
 ## Importance of religiosity ----------------------------------------------
 
-
+attributes(raw_data$dc22_religion_import)
+table(raw_data$dc22_religion_import, useNA = "always")
+clean_data$importance <- NA
+clean_data$importance[raw_data$dc22_religion_import == 1] <- 1
+clean_data$importance[raw_data$dc22_religion_import == 2] <- 0.67
+clean_data$importance[raw_data$dc22_religion_import == 3] <- 0.33
+clean_data$importance[raw_data$dc22_religion_import == 4] <- 0
+table(clean_data$importance)
 
 
 # Aggregate --------------------------------------------------------------
 
 #### inclure ici entre guillemets les noms des variables qui nous intéressent (exemple: importance, attend, etc.)
-variables <- c()
+variables <- c("importance")
 
 output <- clean_data |> 
   tidyr::pivot_longer(
@@ -37,11 +48,11 @@ output <- clean_data |>
     names_to = "variable_id",
     values_to = "choice"
   ) |> 
-  group_by(subgroup, variable_id, choice) |> 
+  group_by(subgroup, variable, value) |> 
   summarise(
     n = n()
   ) |> 
-  group_by(subgroup, variable_id) |> 
+  group_by(subgroup, variable) |> 
   mutate(value = n / sum(n)) |> 
   select(-n)
 
