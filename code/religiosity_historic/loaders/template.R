a0b813da5bd25c6071c5666c072391599c30ccd8
hubcad25 <hubertcadieux@gmail.com>
2024-08-07 12:24:41 -0400

    template for religiosity historic

---
+++ code/religiosity_historic/loaders/template.R
@@ -0,0 +1,54 @@
+# Packages ---------------------------------------------------------------
+library(dplyr)
+
+# Load Raw Data -------------------------------------------------------------------
+
+raw_data
+
+
+# Create Clean Data ------------------------------------------------------
+
+clean_data <- data.frame(
+  id = 1:nrow(raw_data)
+)
+
+# Clean relevant variables -----------------------------------------------
+
+
+## Quebec-ROC -----------------------------------------------------------------
+
+### name this variable "subgroup"
+#### categories : qc, roc
+
+
+## Importance of religiosity ----------------------------------------------
+
+
+
+
+# Aggregate --------------------------------------------------------------
+
+#### inclure ici entre guillemets les noms des variables qui nous intéressent (exemple: importance, attend, etc.)
+variables <- c()
+
+output <- clean_data |> 
+  tidyr::pivot_longer(
+    cols = variables,
+    names_to = "variable",
+    values_to = "value"
+  ) |> 
+  group_by(subgroup, variable, value) |> 
+  summarise(
+    n = n()
+  ) |> 
+  group_by(subgroup, variable) |> 
+  mutate(prop = n / sum(n))
+
+
+# Save -------------------------------------------------------------------
+
+### fill the survey_id variable
+survey_id <- ""
+
+### save it in the warehouse
+saveRDS(output, paste0("_SharedFolder_article_religion-magie/Data/religiosity_historic/warehouse/individual/", survey_id, ".rds"))
