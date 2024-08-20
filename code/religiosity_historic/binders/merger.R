137a7f00a285e3ed4d602812e61867184699b336
capel201 <cam.pell@me.com>
2024-08-20 12:52:50 -0400

    saveRDS

--- code/religiosity_historic/binders/merger.R
+++ code/religiosity_historic/binders/merger.R
@@ -35,3 +35,6 @@ general_social_survey_2020 <- readRDS("_SharedFolder_article_religion-magie/Data
 
 combined_df <- rbind(cora_cpep, democracy_checkup_2019, democracy_checkup_2020, democracy_checkup_2021, democracy_checkup_2022, environics_2011, general_social_survey_2010, general_social_survey_2011, general_social_survey_2013, general_social_survey_2014, general_social_survey_2015, general_social_survey_2016, general_social_survey_2017, general_social_survey_2018, general_social_survey_2020)
 
+
+saveRDS(combined_df, "")
+
