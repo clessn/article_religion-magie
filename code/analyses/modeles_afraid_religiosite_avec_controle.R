6228f31c727679dee7684b0f0ed9ae781e107457
LaurenceOMFoisy <mail@mfoisy.com>
2025-02-11 18:24:28 -0500

    add png to repo

--- code/analyses/modeles_afraid_religiosite_avec_controle.R
+++ code/analyses/modeles_afraid_religiosite_avec_controle.R
@@ -45,30 +45,30 @@ data_quorum_roc$religion_bin <- as.numeric(data_quorum_roc$religion_bin)
 
 models_list <- list(
   '\\textbf{Only religious respondents}' = list(
-    "Canada" = survey::svyglm(religion_attached_to_church_religious ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum, weights = ~weight)),
-    "Quebec" = survey::svyglm(religion_attached_to_church_religious ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_qc, weights = ~weight)),
-    "Rest of Canada" = survey::svyglm(religion_attached_to_church_religious ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_roc, weights = ~weight))
+    "Canada" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_religious + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum, weights = ~weight)),
+    "Quebec" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_religious + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_qc, weights = ~weight)),
+    "Rest of Canada" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_religious + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_roc, weights = ~weight))
   ),
   '\\textbf{All respondents}' = list(
-    "Canada" = survey::svyglm(religion_attached_to_church_all ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum, weights = ~weight)),
-    "Quebec" = survey::svyglm(religion_attached_to_church_all ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_qc, weights = ~weight)),
-    "Rest of Canada" = survey::svyglm(religion_attached_to_church_all ~ covid_afraid_of_dying + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_roc, weights = ~weight))
+    "Canada" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_all + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum, weights = ~weight)),
+    "Quebec" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_all + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_qc, weights = ~weight)),
+    "Rest of Canada" = survey::svyglm(covid_afraid_of_dying ~ religion_attached_to_church_all + ses_female + ses_age_group + ses_marital_status + ses_born_canada + ses_education + ses_sexual_orientation + ses_occupation + ses_ethnicity, design = survey::svydesign(ids = ~1, data = data_quorum_roc, weights = ~weight))
   )
 )
 
-# Translation of "Peur de la mort" into English
-cm <- c("covid_afraid_of_dying" = "Fear of death")
-
+# Translation of "Attachement religieux" into English
+cm <- c("religion_attached_to_church_religious" = "Religious attachment (religious respondents)",
+        "religion_attached_to_church_all" = "Religious attachment (all respondents)")
 gm <- c('r.squared', 'nobs')
 
 # Model summary code
 modelsummary::modelsummary(models_list,
-                           output = "code/analyses/reg_table_afraid_avec_controle.tex", 
+                           output = "code/analyses/reg_table_religious_avec_controle.tex", 
                            stars = TRUE,
                            coef_map = cm, 
                            gof_map = gm,
                            shape = "rbind",
-                           title = "Relationship between fear of death during COVID-19 and religiosity",
+                           title = "Relationship between religiosity and fear of death during COVID-19",
                            notes = "Note: The controls used in these models are as follows: sex, age group, marital status, place of birth, education, sexual orientation, occupation, and ethnicity.")
 
-reg_table_path <- latex_table_to_png("code/analyses/reg_table_afraid_avec_controle.tex", output_path = "_SharedFolder_article_religion-magie/Data/tables/reg_table.png", is_file = TRUE, dpi = 600)
+reg_table_path <- latex_table_to_png("code/analyses/reg_table_religious_avec_controle.tex", output_path = "_SharedFolder_article_religion-magie/Data/tables/reg_table.png", is_file = TRUE, dpi = 600)
