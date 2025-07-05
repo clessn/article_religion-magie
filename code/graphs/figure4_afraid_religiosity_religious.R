1534309fad0872052984808f456ae30909ecd8fb
LaurenceOMFoisy <mail@mfoisy.com>
2025-07-05 16:08:15 -0400

    grind

--- code/graphs/figure4_afraid_religiosity_religious.R
+++ code/graphs/figure4_afraid_religiosity_religious.R
@@ -19,13 +19,13 @@ graph_data <- data %>%
   tidyr::drop_na(region, religion_attached_to_church_religious, covid_afraid_of_dying) %>%
   mutate(region = ifelse(region == "qc", "Quebec", "Rest of Canada"))
 
-graph_data %>%
-  group_by(region, religion_attached_to_church_religious, covid_afraid_of_dying) %>% 
+p <- graph_data %>%
+  group_by(region, covid_afraid_of_dying, religion_attached_to_church_religious) %>% 
   summarise(n = n()) %>% 
   group_by(region) %>% 
   mutate(prop = n / sum(n) * 100) %>%
-  ggplot(aes(y = religion_attached_to_church_religious,
-             x = covid_afraid_of_dying,
+  ggplot(aes(y = covid_afraid_of_dying,
+             x = religion_attached_to_church_religious,
              color = region, fill = region)) +
   facet_wrap(~region) +
   geom_tile(aes(alpha = prop),
@@ -35,21 +35,23 @@ graph_data %>%
               se = FALSE, alpha = 0.1) +
   clessnize::theme_clean_light(base_size = 15) +
   scale_y_continuous(breaks = c(0.2, 0.8),
-                     labels = c("Less attached", "More attached")) +
-  scale_x_continuous(breaks = c(0.2, 0.8),
                      labels = c("Less afraid", "More afraid")) +
+  scale_x_continuous(breaks = c(0.2, 0.8),
+                     labels = c("Less attached", "More attached")) +
   scale_fill_manual(values = c("#003DA5", "#D80621"),
                     labels = c("Quebec", "Rest of Canada")) +
   scale_color_manual(values = c("#003DA5", "#D80621"),
                      labels = c("Quebec", "Rest of Canada")) +
   scale_alpha_continuous(range = c(0.02, 0.8),
                          name = "Proportion (%)") +
-  labs(y = "Attachment to\nReligion\n",
-       x = "\nAfraid of Dying from COVID-19",
+  labs(y = "\nAfraid of Dying from COVID-19",
+       x = "Attachment to\nReligion\n",
        caption = "Among respondents who are attached to a religion.") +
   guides(color = "none", fill = "none") +
   theme(legend.title = element_text(),
         axis.text.y = element_text(angle = 90, hjust = 0.5))
+
+print(p)
 
 ggsave("_SharedFolder_article_religion-magie/figures/figure4_afraid_religiosity_religious.png",
        width = 10, height = 6, dpi = 300)
